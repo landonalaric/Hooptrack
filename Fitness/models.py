50f9c01f262080e64e5e520381f3faeca9af4ffa
landonalaric <hemsworthdavid355@gmail.com>
2026-08-17 13:41:34 +0300

    s

--- Fitness/models.py
+++ Fitness/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+
 from django.db import models
 from players.models import Player
 # Create your models here.
--- Fitness/views.py
+++ Fitness/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 from rest_framework import viewsets, permissions, exceptions
+
 from Fitness.serializer import FitnessLogSerializer
 from .models import FitnessLog
 
 from accounts.models import Role, GuardianProfile
+
 
 class FitnessLogViewSet(viewsets.ModelViewSet):
     serializer_class = FitnessLogSerializer
@@ -11,17 +12,35 @@ class FitnessLogViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
+
         if user.role == Role.SUPERADMIN:
             return FitnessLog.objects.all()
+
         elif user.role == Role.GUARDIAN:
             try:
-                profile = GuardianProfile.objects.get(user=user)
-                return FitnessLog.objects.filter(player__player_code=profile.player_code, player__academy=user.academy)
+                profile = GuardianProfile.objects.get(
+                    user=user
+                )
+
+                return FitnessLog.objects.filter(
+                    player__player_code=profile.player_code,
+                    player__academy=user.academy
+                )
+
             except GuardianProfile.DoesNotExist:
                 return FitnessLog.objects.none()
-        return FitnessLog.objects.filter(player__academy=user.academy)
+
+        return FitnessLog.objects.filter(
+            player__academy=user.academy
+        )
 
     def perform_create(self, serializer):
-        if self.request.user.role not in [Role.ADMIN, Role.COACH]:
-            raise exceptions.PermissionDenied("Only coaches and admins can register fitness and injury reports.")
+        if self.request.user.role not in [
+            Role.ADMIN,
+            Role.COACH
+        ]:
+            raise exceptions.PermissionDenied(
+                "Only coaches and admins can register fitness and injury reports."
+            )
+
         serializer.save()

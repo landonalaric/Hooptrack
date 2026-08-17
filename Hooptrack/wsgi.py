33e23d51ea2ed2cd42c0c27949c34dedd83a9f95
landonalaric <hemsworthdavid355@gmail.com>
2026-08-17 11:38:08 +0300

    wsgi

--- Hooptrack/wsgi.py
+++ Hooptrack/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
-
+sys.path.append("/home/landonalaricv/www/Hooptrack")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hooptrack.settings')
 
 application = get_wsgi_application()

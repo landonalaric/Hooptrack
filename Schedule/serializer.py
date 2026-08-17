ca8ab0c46adb228a56621026da0e96e27363cb96
landonalaric <hemsworthdavid355@gmail.com>
2026-08-17 13:01:58 +0300

    v

--- Schedule/serializer.py
+++ Schedule/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Schedule
 from Teams.models import Team
 
+
 class ScheduleSerializer(serializers.ModelSerializer):
     team = serializers.SlugRelatedField(
         slug_field='name',
